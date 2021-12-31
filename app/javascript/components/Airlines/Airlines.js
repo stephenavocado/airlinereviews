610b91d93be29b173a80598e46c76987ca1da4d7
stephenavocado <stephenwalter@chicagobooth.edu>
2021-12-31 16:16:46 +0000

    removed all turbolinks references and gem

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -3,20 +3,43 @@ import axios from 'axios'
 import Airline from './Airline'
 import styled from 'styled-components'
 
+const Home = styled.div`
+  text-align: center;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+`
+
+const Header = styled.div`
+  padding: 100px 100px 10px 100px;
+
+  h1 {
+    font-size: 42px;
+  }
+`
+const SubHeader = styled.div`
+  font-weight: 300; 
+  font-size: 26px;
+`
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px; 
+  width: 100%;
+  padding: 20px;
+`
+
 const Airlines = () => {
   const [airlines, setAirlines] = useState([])
 
   useEffect(() => {
     // Get airlines from api
-    // update airlines in our state
-
     axios.get('/api/v1/airlines.json')
-      .then( resp => {
-        setAirlines(resp.data.data)
-      })
+      .then( resp => setAirlines(resp.data.data))
       .catch( resp => console.log(resp) )
   }, [airlines.length])
 
+  // update airlines in our state
   const grid = airlines.map(item => {
     return (
       <Airline 
@@ -27,15 +50,13 @@ const Airlines = () => {
   })
   
   return(
-    <div className='home'>
-      <div className="header">
+    <Home>
+      <Header>
         <h1>OpenFlights</h1>
-        <div className='subheader'>Honest, unbiased airline reviews.</div> 
-      </div>
-      <div className='grid'>
-        <ul>{grid}</ul>
-      </div>
-    </div>
+        <SubHeader>Honest, unbiased airline reviews.</SubHeader> 
+      </Header>
+      <Grid>{grid}</Grid>
+    </Home>
   )
 }
 
