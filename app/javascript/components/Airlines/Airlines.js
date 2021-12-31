8448e37176c4fa1179e3d493b8aebf6b2f270e70
stephenavocado <stephenwalter@chicagobooth.edu>
2021-12-31 15:40:42 +0000

    added axios, fetched airlines from api, stored in variable

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
 
 const Airlines = () => {
+  const [airlines, setAirlines] = useState([])
+
+  useEffect(() => {
+    // Get airlines from api
+    // update airlines in our state
+
+    axios.get('/api/v1/airlines.json')
+      .then( resp => {
+        setAirlines(resp.data.data)
+      })
+      .catch( resp => console.log(resp) )
+  }, [airlines.length])
+
+  const list = airlines.map(item => {
+    return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+  })
+  
   return(
+    <Fragment>
     <div>This is Airlines index</div>
+    <ul>{list}</ul>
+    </Fragment>
   )
 }
 
