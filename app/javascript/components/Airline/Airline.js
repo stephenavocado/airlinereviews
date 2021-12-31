caddb06b5c356308f6e18eb2ce0d511d485e0289
stephenavocado <stephenwalter@chicagobooth.edu>
2021-12-31 17:16:28 +0000

    added form component

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,6 +1,26 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, Fragment} from 'react'
 import axios from 'axios'
 import Header from './Header'
+import ReviewForm from './ReviewForm'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`
+const Column = styled.div`
+  background: #fff;
+  height: 100vh;
+
+  &:last-child {
+    background: #000;
+  }
+`
+const Main = styled.div`
+  padding-left: 50px;
+`
 
 const Airline = (props) => {
   const [airline, setAirline] = useState({})
@@ -23,21 +43,25 @@ const Airline = (props) => {
   }, [])
 
   return(
-    <div className='wrapper'>
-      <div className='column'>
-        { 
+    <Wrapper>
+      { 
         loaded &&
-        <Header 
-          attributes={airline.data.attributes}
-          reviews={airline.included}
-        />
-        }
-        <div className='reviews'></div>
-      </div>
-      <div className='column'>
-        <div className='review-form'>[Review form here]</div>
-      </div>
-    </div>
+        <Fragment>
+        <Column>
+          <Main> 
+            <Header 
+              attributes={airline.data.attributes}
+              reviews={airline.included}
+            />
+            <div className='reviews'></div>
+          </Main>
+        </Column>
+        <Column>
+          <ReviewForm>[Review form here]</ReviewForm>
+        </Column>
+        </Fragment>
+      }
+    </Wrapper>
   ) 
 }
 
--- app/javascript/components/Airline/Header.js
+++ app/javascript/components/Airline/Header.js
@@ -1,18 +1,41 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  padding: 50px 100px 50px 0px;
+  font-size: 30px;
+
+  img {
+    height: 60px;
+    width: 60px;
+    border-radius: 100%;
+    border: 1px solid rgba(0,0,0,0.1);
+    margin-botton: -8px;
+  }
+`
+const TotalReviews = styled.div`
+  font-size: 18px;
+  padding: 10px 0;
+`
+const TotalOutOf = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+  padding: 10px 0;
+`
 
 const Header = (props) => {
   const { name, image_url, avg_score } = props.attributes
   const total = props.reviews.length
 
   return (
-    <div className='wrapper'>
+    <Wrapper>
       <h1><img src={image_url} alt={name} />{name}</h1>
       <div>
-        <div className='total-reviews'>{total} User Reviews</div>
+        <TotalReviews>{total} User Reviews</TotalReviews>
         <div className='star-rating'></div>
-        <div className='total-out-of'>{avg_score} out of 5</div>
+        <TotalOutOf>{avg_score} out of 5</TotalOutOf>
       </div>
-    </div>
+    </Wrapper>
   )
 }
 
