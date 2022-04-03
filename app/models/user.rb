cee53796239a7a65014a41da11d405e3a574d3fd
stephenavocado <stephenwalter@chicagobooth.edu>
2022-04-03 19:03:04 +0000

    food journal today

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,12 @@ class User < ApplicationRecord
 
   enum role: [:standard, :admin]
   enum program_pace_goal: [:slow, :medium, :fast]
+
+  def food_journal_today
+    if 
+      self.food_journals.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).count !== 0
+      return true
+    end
+  end
+
 end
